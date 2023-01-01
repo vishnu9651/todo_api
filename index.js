16c86b2f20aa5477ab83e49549946183ad68fa92
vishnu9651 <vishnukantee0044@gmail.com>
2023-01-01 21:09:16 +0530

    apiset

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const jsonServer=require("json-server");
 const server=jsonServer.create();
-const router=jsonServer.defaults();
+const router=jsonServer.router("db.json");
 const middlewares=jsonServer.defaults()
 const port=process.env.PORT || 3001;
 server.use(middlewares);
